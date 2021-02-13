9b103ff302fcf296cb25fdd763adc53ea3d1b204
YeoSangPil <ssi0316128@gmail.com>
2021-02-14 01:43:31 +0900

    add todo, but bug not fixed

--- gretting.js
+++ gretting.js
@@ -5,6 +5,23 @@ const gretting = document.querySelector('.js-gretting');
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currnetValue = input.value;
+    paintGretting(currnetValue);
+    saveName(currnetValue);
+
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGretting(text) {
     form.classList.remove(SHOWING_CN);
     gretting.classList.add(SHOWING_CN);
@@ -14,7 +31,7 @@ function paintGretting(text) {
 function loadName(){
     const userName = localStorage.getItem(USER_LS);
     if(userName === null) {
-
+        askForName();
     } else {
         paintGretting(userName);
     }
